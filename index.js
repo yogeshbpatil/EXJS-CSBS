71a6184573fc094a0238cffef2388db60a7cb2f1
yogeshbpatil <patilyogeshkumarb@gmail.com>
2025-10-05 14:43:54 +0530

    fix: css issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const app = express();
 const absPath = path.resolve('view')
-const publicPath = path.resolve('public');
+const publicPath = path.resolve('view/public'); // Fixed this line
 
 // Serve static files from public directory
 app.use(express.static(publicPath));
