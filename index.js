f87933e3c8e2736ae64b6410e19e6447ad50f688
yogeshbpatil <patilyogeshkumarb@gmail.com>
2025-10-12 00:42:58 +0530

    fix: fetch data from db

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-import express from 'express';
-import { handleUsers } from './controller/userController.js';
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const dbName = "school";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
 const app = express();
 
-app.set('view engine', 'ejs')
-app.get('/users', handleUsers)
+app.set("view engine", "ejs");
+app.get("/", async (req, resp) => {
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection("students");
 
-app.listen(3200)
+  const students = await collection.find().toArray();
+  console.log(students);
+
+  resp.render("students.ejs", { students });
+});
+app.listen(3200);
--- model/userModel.js
+++
@@ -1,3 +0,0 @@
-export function usersList() {
-    return ['Anil', 'Sam', 'Peter', 'Bruce', 'Tony']
-}
